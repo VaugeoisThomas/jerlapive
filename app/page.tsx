9ef93c791a81cc633b0a68fb8a7dba775449412e
Vaugeois Thomas <61732331+VaugeoisThomas@users.noreply.github.com>
2026-04-06 21:37:59 +0200

    Update page.tsx
    Add a description of about me !

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,12 @@ export default function App () {
             <div className="flex flex-col">
               <h1 className="mb-5 text-center">A propos de moi</h1>
               <div id="About" className="bg-foreground text-gray-background">
-                <p className="p-2">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ex aut non dolor eligendi temporibus, placeat ipsum eveniet? Quis eos eius repellendus, minus corrupti et praesentium consectetur voluptatum consequatur, nam aut.</p>
+                <p className="p-2">
+                  Streamer de 39 ans, je suis amoureux des jeux vidéos depuis que je suis petit.<br/>
+                  Je suis un joueur de pas mal de style de jeux vidéo, même si je reste accro aux jeux d&#39;horreur.<br />
+                  Principalement je joue à Phasmophobia, mais je joue à toute sorte de jeux, comme Ghost Janitor, Subliminal ...<br />
+                  N&#39;hésitez pas à venir nous rejoindre en direct sur <a href="https://www.twitch.tv/jerlapive"> Twitch</a>
+                </p>
               </div>
             </div>
             <div className="flex flex-col">
