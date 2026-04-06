c776be5509b7e364d148d7c957f0cb552794d641
Vaugeois Thomas <61732331+VaugeoisThomas@users.noreply.github.com>
2026-04-06 18:13:54 +0200

    Update WindowTwitch.tsx

--- app/components/Body/WindowTwitch.tsx
+++ app/components/Body/WindowTwitch.tsx
@@ -2,9 +2,9 @@ export default function WindowTwitch () {
   return (
     <>
       <iframe 
-        src="https://player.twitch.tv/?channel=jerlapive&parent=jerlapive.vercel.app/" 
+        src="https://player.twitch.tv/?channel=jerlapive&parent=jerlapive.vercel.app" 
         width = "500"
-        height = "250"
+        height = "300"
       />
     </>
   )
