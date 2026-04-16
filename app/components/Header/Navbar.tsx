fa9396967f825737afea63909580a12354ae7a76
Vaugeois Thomas <61732331+VaugeoisThomas@users.noreply.github.com>
2026-04-16 17:39:04 +0200

    refactoring Link

--- app/components/Header/Navbar.tsx
+++ app/components/Header/Navbar.tsx
@@ -2,9 +2,10 @@ import Image from 'next/image'
 import Link from 'next/link'
 
 export default function Navbar() {
+
   return (
     <nav className="flex justify-between items-center p-2 max-h-20 
-    bg-linear-45 from-black-light to-black-lighter">
+     bg-linear-45 from-black-light to-black-lighter shadow-md shadow-green-shadow">
       <Link href="/">
         <Image
           src="/jerlapive.png"
@@ -16,9 +17,9 @@ export default function Navbar() {
         />
       </Link>
       <ul className="flex items-center gap-4 px-4">
-        <Link className="hover:bg-gray-background hover:rounded-2xl hover:shadow-md shadow-green-shadow py-4 px-4" href="/games">Mes jeux</Link>
-        <Link className="hover:bg-gray-background hover:rounded-2xl hover:shadow-md shadow-green-shadow py-4 px-4" href="/phasmophobia">Phasmophobia</Link>
-        <Link className="hover:bg-gray-background hover:rounded-2xl hover:shadow-md shadow-green-shadow py-4 px-4" href="/challenge">Mes défis</Link>
+        <a className="nav-link" href="/games">Mes jeux</a>
+        <a className="nav-link" href="/phasmophobia">Phasmophobia</a>
+        <a className="nav-link" href="/challenge">Mes défis</a>
       </ul>
     </nav>
   )
--- app/page.tsx
+++ app/page.tsx
@@ -6,25 +6,27 @@ import Card from './components/Body/Card'
 export default function App() {
   return (
     <>
-      <Header/>
-       <Body>
+      <Header />
+      {/* <Body>
         <main className="flex flex-col m-3 items-center">
-           <WindowTwitch />
-           <div className="flex flex-row justify-between gap-5" >
+          <WindowTwitch />
+          <div className="flex flex-row justify-between mt-20 gap-2">
             <Card title="A propos de moi" id='About'>
-                <p className="p-2">
-                  Streamer de 39 ans, je suis amoureux des jeux vidéos depuis que je suis petit.<br />
-                  Je suis un joueur de pas mal de style de jeux vidéo, même si je reste accro aux jeux d&#39;horreur.<br />
-                  Principalement je joue à Phasmophobia, mais je joue à toute sorte de jeux, comme Ghost Janitor, Subliminal ...<br />
-                  N&#39;hésitez pas à venir nous rejoindre en direct sur <a href="https://www.twitch.tv/jerlapive"> Twitch</a>
-                </p>
+              <p className="p-2">
+                Streamer de 39 ans, je suis amoureux des jeux vidéos depuis que je suis petit.<br />
+                Je suis un joueur de pas mal de style de jeux vidéo, même si je reste accro aux jeux d&#39;horreur.<br />
+                Principalement je joue à Phasmophobia, mais je joue à toute sorte de jeux, comme Ghost Janitor, Subliminal ...<br />
+                N&#39;hésitez pas à venir nous rejoindre en direct sur <a href="https://www.twitch.tv/jerlapive"> Twitch</a>
+              </p>
             </Card>
             <Card title="Mes dernières vidéos" id="lastVideo">
-
+              <p className="p-2">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Necessitatibus, tempora nulla accusantium dignissimos eveniet corrupti labore optio. Dicta, porro maxime officia aut sapiente eos, earum nostrum distinctio consectetur minima nihil.
+              </p>
             </Card>
           </div>
-        </main> 
-      </Body>
+        </main>
+      </Body> */}
     </>
   )
 }
